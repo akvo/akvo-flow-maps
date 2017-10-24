95bca89acb909553ffd3f84d5b13babf304002a6
Dan Lebrero <dlebrero@gmail.com>
2017-10-24 17:33:36 +0200

    inserting only valid data points

--- src/akvo/flow/maps/boundary/db.clj
+++ src/akvo/flow/maps/boundary/db.clj
@@ -5,7 +5,8 @@
 
 (def jdbc "jdbc:postgresql://postgres/a_tenant_db?user=a_tenant_user&password=a_tenant_password")
 
-(jdbc/execute! jdbc ["DELETE from datapoint"])
+(comment
+  (jdbc/execute! jdbc ["DELETE from datapoint"]))
 
 (defn ->db-timestamp [v]
   (when v
@@ -17,19 +18,25 @@
       (update :created-date-time ->db-timestamp)
       (update :last-update-date-time ->db-timestamp)))
 
+(defn valid? [record]
+  (and (:id record)
+       (:latitude record)
+       (:longitude record)
+       (:survey-id record)))
+
 (defn insert-batch [datapoints]
-  (when (seq datapoints)
+  (when-let [db-datapoints (->> datapoints
+                                (map ->db-value)
+                                (filter valid?)
+                                (map (juxt :id :survey-id :last-update-date-time :created-date-time :latitude :longitude
+                                           :survey-id :last-update-date-time :created-date-time :latitude :longitude :id))
+                                seq)]
     (jdbc/execute! jdbc
                    (into ["insert into datapoint(id, survey_id, created_date_time, last_update_date_time, geom)
                                  values (?, ?, ?, ?, ST_SetSRID(ST_MakePoint(?, ?), 4326))
                                  ON conflict(id)
                          do update set (survey_id, created_date_time, last_update_date_time, geom) = (?, ?, ?, ST_SetSRID(ST_MakePoint(?, ?), 4326))
                             where datapoint.id = ?"]
-                         (map
-                           (comp
-                             (juxt :id :survey-id :last-update-date-time :created-date-time :latitude :longitude
-                                   :survey-id :last-update-date-time :created-date-time :latitude :longitude :id)
-                             ->db-value)
-                           datapoints))
+                         db-datapoints)
                    {:transaction? true
                     :multi?       true})))
