eb4dafde2eb3f07c13da66bf103f99b6f3425599
Dan Lebrero <dlebrero@gmail.com>
2017-10-24 17:20:18 +0200

    creating a tenant db

--- postgres/provision/helpers/create-database.sql
+++ postgres/provision/helpers/create-database.sql
@@ -1,5 +1,12 @@
-CREATE DATABASE flow_maps
+CREATE DATABASE master_db
 WITH OWNER = dbuser
+     TEMPLATE = template0
+     ENCODING = 'UTF8'
+     LC_COLLATE = 'en_US.UTF-8'
+     LC_CTYPE = 'en_US.UTF-8';
+
+CREATE DATABASE a_tenant_db
+WITH OWNER = a_tenant_user
      TEMPLATE = template0
      ENCODING = 'UTF8'
      LC_COLLATE = 'en_US.UTF-8'
