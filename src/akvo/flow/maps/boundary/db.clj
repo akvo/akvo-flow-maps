f63db2182aa8236c66c016580e80f49f965c2c52
Dan Lebrero <dlebrero@gmail.com>
2017-10-24 17:21:02 +0200

    upserting a batch of messages into the db

---
+++ src/akvo/flow/maps/boundary/db.clj
@@ -0,0 +1,35 @@
+(ns akvo.flow.maps.boundary.db
+  (:require
+    clojure.set
+    [clojure.java.jdbc :as jdbc]))
+
+(def jdbc "jdbc:postgresql://postgres/a_tenant_db?user=a_tenant_user&password=a_tenant_password")
+
+(jdbc/execute! jdbc ["DELETE from datapoint"])
+
+(defn ->db-timestamp [v]
+  (when v
+    (java.sql.Timestamp. v)))
+
+(defn ->db-value [record]
+  (-> record
+      (clojure.set/rename-keys {:identifier :id})
+      (update :created-date-time ->db-timestamp)
+      (update :last-update-date-time ->db-timestamp)))
+
+(defn insert-batch [datapoints]
+  (when (seq datapoints)
+    (jdbc/execute! jdbc
+                   (into ["insert into datapoint(id, survey_id, created_date_time, last_update_date_time, geom)
+                                 values (?, ?, ?, ?, ST_SetSRID(ST_MakePoint(?, ?), 4326))
+                                 ON conflict(id)
+                         do update set (survey_id, created_date_time, last_update_date_time, geom) = (?, ?, ?, ST_SetSRID(ST_MakePoint(?, ?), 4326))
+                            where datapoint.id = ?"]
+                         (map
+                           (comp
+                             (juxt :id :survey-id :last-update-date-time :created-date-time :latitude :longitude
+                                   :survey-id :last-update-date-time :created-date-time :latitude :longitude :id)
+                             ->db-value)
+                           datapoints))
+                   {:transaction? true
+                    :multi?       true})))
