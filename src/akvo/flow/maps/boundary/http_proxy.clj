297d72987243574417bd781389e0a1d81b426e0a
Dan Lebrero <dlebrero@gmail.com>
2017-10-26 15:53:26 +0200

    [#1] a simple proxy

---
+++ src/akvo/flow/maps/boundary/http_proxy.clj
@@ -0,0 +1,26 @@
+(ns akvo.flow.maps.boundary.http-proxy
+  (:require [http.async.client :as http]
+            [http.async.client.request :as http-req])
+  (:import (com.ning.http.client Request)))
+
+(defn proxy-request [client {:keys [method url] :as req}]
+  (let [request ^Request (apply http-req/prepare-request method url (apply concat (dissoc req :method :url)))
+        response (http/await (http-req/execute-request client request))]
+    (assoc response
+      :status (http/status response)
+      :body (http/string response)
+      :error (http/error response)
+      :headers (http/headers response))))
+
+(defn create-client [{:keys [connection-timeout request-timeout max-connections] :as config}]
+  {:pre [connection-timeout request-timeout max-connections]}
+  (http/create-client
+    :connection-timeout connection-timeout
+    :request-timeout request-timeout
+    :read-timeout request-timeout
+    :max-conns-per-host max-connections
+    :max-conns-total max-connections
+    :idle-in-pool-timeout 60000))
+
+(defn destroy [client]
+  (http/close client))
