a7b64e1e0b82440bf17e7e914db08c815fd19cdc
Dan Lebrero <dlebrero@gmail.com>
2017-10-26 15:48:03 +0200

    commenting out consumer so it is not run during repl/reset

--- src/akvo/flow/maps/boundary/consumer_wip.clj
+++ src/akvo/flow/maps/boundary/consumer_wip.clj
@@ -8,31 +8,32 @@
   (:import (io.confluent.kafka.serializers KafkaAvroDeserializer)
            (org.apache.kafka.clients.consumer OffsetCommitCallback)))
 
-(def c (consumer/make-consumer
-         {:bootstrap.servers       "kafka:29092"
-          :group.id                "the-consumer-test-101"
-          :client.id               "example-consumer_host_name_or_container_id"
-          :auto.offset.reset       :earliest
-          :enable.auto.commit      true
-          :max.poll.records        100
-          :auto.commit.interval.ms 10000}
-         (deserializers/long-deserializer)
-         (doto
-           (KafkaAvroDeserializer.)
-           (.configure {"schema.registry.url" "http://schema-registry:8081"} false))
-         {:poll-timeout-ms        1000
-          :offset-commit-callback (reify OffsetCommitCallback
-                                    (onComplete [this map e]
-                                      (println "offset commited!" map e)))}))
+(comment
+  (def c (consumer/make-consumer
+           {:bootstrap.servers       "kafka:29092"
+            :group.id                "the-consumer-test-101"
+            :client.id               "example-consumer_host_name_or_container_id"
+            :auto.offset.reset       :earliest
+            :enable.auto.commit      true
+            :max.poll.records        100
+            :auto.commit.interval.ms 10000}
+           (deserializers/long-deserializer)
+           (doto
+             (KafkaAvroDeserializer.)
+             (.configure {"schema.registry.url" "http://schema-registry:8081"} false))
+           {:poll-timeout-ms        1000
+            :offset-commit-callback (reify OffsetCommitCallback
+                                      (onComplete [this map e]
+                                        (println "offset commited!" map e)))}))
 
 
-(cp/subscribe-to-partitions! c #".*datapoint.*")
+  (cp/subscribe-to-partitions! c #".*datapoint.*")
 
-(dotimes [_ 10]
-  (let [records (cp/poll! c)
-        batch (into [] (map (fn [r]
-                              (update r :value avro/->clj))) records)]
-    (prn "batch:" (count batch)
-         "db:" (db/insert-batch (map :value batch)))))
+  (dotimes [_ 10]
+    (let [records (cp/poll! c)
+          batch (into [] (map (fn [r]
+                                (update r :value avro/->clj))) records)]
+      (prn "batch:" (count batch)
+           "db:" (db/insert-batch (map :value batch)))))
 
-(.close c)
+  (.close c))
