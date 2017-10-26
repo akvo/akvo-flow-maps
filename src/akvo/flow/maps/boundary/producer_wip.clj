14db4160ad4e7330ceb1c8839574f6bf6a7de234
Dan Lebrero <dlebrero@gmail.com>
2017-10-26 15:48:16 +0200

    commenting out producer so it is not run during repl/reset

--- src/akvo/flow/maps/boundary/producer_wip.clj
+++ src/akvo/flow/maps/boundary/producer_wip.clj
@@ -9,44 +9,45 @@
     [franzy.serialization.serializers :as serializers])
   (:import (io.confluent.kafka.serializers KafkaAvroSerializer)))
 
-(def p (producer/make-producer {:bootstrap.servers "kafka:29092"
-                                :acks              "all"
-                                :retries           1
-                                :client.id         "example-producer"
-                                }
-                               (serializers/long-serializer)
-                               (doto
-                                 (KafkaAvroSerializer.)
-                                 (.configure {"schema.registry.url" "http://schema-registry:8081"} false))))
+(comment
+  (def p (producer/make-producer {:bootstrap.servers "kafka:29092"
+                                  :acks              "all"
+                                  :retries           1
+                                  :client.id         "example-producer"
+                                  }
+                                 (serializers/long-serializer)
+                                 (doto
+                                   (KafkaAvroSerializer.)
+                                   (.configure {"schema.registry.url" "http://schema-registry:8081"} false))))
 
-(def DataPointSchema
-  (avro/parse-schema
-    (json/generate-string
-      {:namespace "org.akvo.flow",
-       :type      "record",
-       :name      "DataPoint",
-       :fields    [
-                   {:name "identifier", :type "string"}
-                   {:name "survey_id", :type ["null" "long"]}
-                   {:name "longitude", :type ["null" "double"]}
-                   {:name "latitude", :type ["null" "double"]}
-                   ;; TODO: avro-bridge is playing around with the cases
-                   {:name "created_date_time", :type "long"}
-                   {:name "last_update_date_time", :type "long"}]})))
+  (def DataPointSchema
+    (avro/parse-schema
+      (json/generate-string
+        {:namespace "org.akvo.flow",
+         :type      "record",
+         :name      "DataPoint",
+         :fields    [
+                     {:name "identifier", :type "string"}
+                     {:name "survey_id", :type ["null" "long"]}
+                     {:name "longitude", :type ["null" "double"]}
+                     {:name "latitude", :type ["null" "double"]}
+                     ;; TODO: avro-bridge is playing around with the cases
+                     {:name "created_date_time", :type "long"}
+                     {:name "last_update_date_time", :type "long"}]})))
 
-(with-open [rdr (clojure.java.io/reader "akvoflowsandbox.SurveyedLocale.edn")]
-  (dorun (for [line (line-seq rdr)]
-           (let [value (-> (read-string line)
-                           (select-keys ["identifier" "surveyGroupId" "longitude" "latitude" "createdDateTime" "lastUpdateDateTime"])
-                           (clojure.set/rename-keys {"surveyGroupId" "surveyId"})
-                           (update "createdDateTime" #(.getTime %))
-                           (update "lastUpdateDateTime" #(.getTime %))
-                           )]
-             (send-async!
-               p
-               {:topic (str "akvoflowsandbox..datapoint")
-                :key   (get value "surveyId")
-                :value (avro/->java DataPointSchema
-                                    value)})))))
+  (with-open [rdr (clojure.java.io/reader "akvoflowsandbox.SurveyedLocale.edn")]
+    (dorun (for [line (line-seq rdr)]
+             (let [value (-> (read-string line)
+                             (select-keys ["identifier" "surveyGroupId" "longitude" "latitude" "createdDateTime" "lastUpdateDateTime"])
+                             (clojure.set/rename-keys {"surveyGroupId" "surveyId"})
+                             (update "createdDateTime" #(.getTime %))
+                             (update "lastUpdateDateTime" #(.getTime %))
+                             )]
+               (send-async!
+                 p
+                 {:topic (str "org.akvo.akvoflowsandbox.datapoint")
+                  :key   (get value "surveyId")
+                  :value (avro/->java DataPointSchema
+                                      value)})))))
 
-(.close p)
+  (.close p))
