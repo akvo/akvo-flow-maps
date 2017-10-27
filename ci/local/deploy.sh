928fa10291339a18b31116d135b0274e2b827546
Dan Lebrero <dlebrero@gmail.com>
2017-10-27 18:08:14 +0200

    [#7] Push images to Google

---
+++ ci/local/deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -e
+
+PROJECT_NAME=akvo-lumen
+
+gcloud container clusters get-credentials test --zone europe-west1-d --project akvo-lumen
+
+#sudo /opt/google-cloud-sdk/bin/gcloud config set container/use_client_certificate True
+
+gcloud docker -- push eu.gcr.io/${PROJECT_NAME}/akvo-flow-maps
+gcloud docker -- push eu.gcr.io/${PROJECT_NAME}/akvo-flow-maps-windshaft
+
+#kubectl apply -f ci/local/deployment.yaml
+#kubectl apply -f ci/redis-master-windshaft.yaml
