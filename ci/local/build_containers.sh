24126f0aef93d3ebc311977fa3b72ffbc904fe63
Dan Lebrero <dlebrero@gmail.com>
2017-10-27 22:42:35 +0200

    [#7] Uncommenting "lein uberjar"

--- ci/local/build_containers.sh
+++ ci/local/build_containers.sh
@@ -5,7 +5,7 @@ set -e
 PROJECT_NAME=akvo-lumen
 CIRCLE_SHA1=any
 
-#lein uberjar
+lein uberjar
 
 docker build --rm=false -t eu.gcr.io/${PROJECT_NAME}/akvo-flow-maps:$CIRCLE_SHA1 .
 docker tag eu.gcr.io/${PROJECT_NAME}/akvo-flow-maps:$CIRCLE_SHA1 eu.gcr.io/${PROJECT_NAME}/akvo-flow-maps:develop
