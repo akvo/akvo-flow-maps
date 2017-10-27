4f96444044bf3c9b2a7f101e56f8f606c4bf7dd3
Dan Lebrero <dlebrero@gmail.com>
2017-10-27 17:42:17 +0200

    [#7] Windshaft to use the flow-maps Redis

--- windshaft/config/prod/environment.js
+++ windshaft/config/prod/environment.js
@@ -1,6 +1,6 @@
 module.exports.credentials_encryption_key= process.env.LUMEN_ENCRYPTION_KEY;
 module.exports.redis = {
-    host: 'redis-master-windshaft',
+    host: 'redis-master-flow-maps',
     max: 10
 };
 module.exports.renderer = {
