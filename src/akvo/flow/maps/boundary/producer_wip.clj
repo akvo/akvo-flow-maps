c389d0bd9ccc4316c198995da7fec9c2967bd156
Dan Lebrero <dlebrero@gmail.com>
2017-10-30 17:55:13 +0100

    [#7] Pushing data using REST proxy

--- src/akvo/flow/maps/boundary/producer_wip.clj
+++ src/akvo/flow/maps/boundary/producer_wip.clj
@@ -20,29 +20,31 @@
                                    (KafkaAvroSerializer.)
                                    (.configure {"schema.registry.url" "http://schema-registry:8081"} false))))
 
-  (def DataPointSchema
-    (avro/parse-schema
-      (json/generate-string
-        {:namespace "org.akvo.flow",
-         :type      "record",
-         :name      "DataPoint",
-         :fields    [
-                     {:name "identifier", :type "string"}
-                     {:name "survey_id", :type ["null" "long"]}
-                     {:name "longitude", :type ["null" "double"]}
-                     {:name "latitude", :type ["null" "double"]}
-                     ;; TODO: avro-bridge is playing around with the cases
-                     {:name "created_date_time", :type "long"}
-                     {:name "last_update_date_time", :type "long"}]})))
+  (def DataPointSchema-as-json (json/generate-string
+                                 {:namespace "org.akvo.flow",
+                                  :type      "record",
+                                  :name      "DataPoint",
+                                  :fields    [
+                                              {:name "identifier", :type "string"}
+                                              {:name "survey_id", :type ["null" "long"]}
+                                              {:name "longitude", :type ["null" "double"]}
+                                              {:name "latitude", :type ["null" "double"]}
+                                              ;; TODO: avro-bridge is playing around with the cases
+                                              {:name "created_date_time", :type "long"}
+                                              {:name "last_update_date_time", :type "long"}]}))
+  (def DataPointSchema (avro/parse-schema DataPointSchema-as-json))
 
+  (defn parse-line [line]
+    (-> (read-string line)
+        (select-keys ["identifier" "surveyGroupId" "longitude" "latitude" "createdDateTime" "lastUpdateDateTime"])
+        (clojure.set/rename-keys {"surveyGroupId" "surveyId"})
+        (update "createdDateTime" #(.getTime %))
+        (update "lastUpdateDateTime" #(.getTime %))))
+
+  ;;; Push through Kafka
   (with-open [rdr (clojure.java.io/reader "akvoflowsandbox.SurveyedLocale.edn")]
     (dorun (for [line (line-seq rdr)]
-             (let [value (-> (read-string line)
-                             (select-keys ["identifier" "surveyGroupId" "longitude" "latitude" "createdDateTime" "lastUpdateDateTime"])
-                             (clojure.set/rename-keys {"surveyGroupId" "surveyId"})
-                             (update "createdDateTime" #(.getTime %))
-                             (update "lastUpdateDateTime" #(.getTime %))
-                             )]
+             (let [value (parse-line line)]
                (send-async!
                  p
                  {:topic (str "org.akvo.akvoflowsandbox.datapoint")
@@ -50,4 +52,35 @@
                   :value (avro/->java DataPointSchema
                                       value)})))))
 
-  (.close p))
+  (.close p)
+
+  ;;; Push through Rest proxy
+  (defn ->avro-json [o]
+    (let [bo (ByteArrayOutputStream.)
+          enc (.jsonEncoder (EncoderFactory/get) DataPointSchema bo)]
+      (.write (GenericDatumWriter. DataPointSchema)
+              (avro/->java DataPointSchema o)
+              enc)
+      (.flush enc)
+      (.close bo)
+      (String. (.toByteArray bo) (Charset/forName "UTF-8"))))
+
+  (with-open [rdr (clojure.java.io/reader "akvoflowsandbox.SurveyedLocale.edn")
+              http-client (akvo.flow.maps.boundary.http-proxy/create-client {:connection-timeout 10000
+                                                                             :request-timeout    20000
+                                                                             :max-connections    2})]
+    (dorun (for [line (line-seq rdr)]
+             (let [value (parse-line line)]
+               (let [r (akvo.flow.maps.boundary.http-proxy/proxy-request
+                         http-client
+                         {:method  :post
+                          :headers {"Content-Type" "application/vnd.kafka.avro.v2+json"
+                                    "Accept"       "application/vnd.kafka.v2+json"}
+                          :url     "http://35.195.81.104:80/topics/org.akvo.akvoflowsandbox3.datapoint"
+                          :body    (json/generate-string {:value_schema DataPointSchema-as-json
+                                                          :records      [{:value (json/parse-string (->avro-json value))}]})})]
+                 (if (or (:error r) (not= (:code (:status r)) 200))
+                   (throw (ex-info "" (assoc r :value value)))
+                   (println (:status r) (:body r))))))))
+
+  )
