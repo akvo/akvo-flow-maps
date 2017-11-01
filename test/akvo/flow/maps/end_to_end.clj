fb07cda5928719551278df25fe66cbbaeaf53716
Dan Lebrero <dlebrero@gmail.com>
2017-11-01 18:19:02 +0100

    [#9] Using env vars in the tests + giving sometime to the environment to be ready

--- test/akvo/flow/maps/end_to_end.clj
+++ test/akvo/flow/maps/end_to_end.clj
@@ -6,7 +6,10 @@
     [cheshire.core :as json]
     [franzy.serialization.serializers :as serializers]
     [akvo.flow.maps.boundary.http-proxy :as http]
-    [clojure.test :refer :all])
+    [clojure.tools.logging :refer [info]]
+    [clojure.test :refer :all]
+    [clojure.java.jdbc :as jdbc]
+    [clojure.test :as test])
   (:import (io.confluent.kafka.serializers KafkaAvroSerializer)
            (java.util UUID)))
 
@@ -22,6 +25,15 @@
                  (Thread/sleep 100)
                  (recur))
                result#))))))
+
+(defn check-db-is-up [f]
+  (try-for 60
+           (jdbc/with-db-connection
+             [conn {:connection-uri (System/getenv "DATABASE_URL")}]
+             (jdbc/query conn ["select * from datapoint"])))
+  (f))
+
+(test/use-fixtures :once check-db-is-up)
 
 (def DataPointSchema-as-json
   (json/generate-string
@@ -41,73 +53,56 @@
 
 (defn push-data-point [data-point]
   (with-open [producer (producer/make-producer
-                         {:bootstrap.servers "kafka:29092"
+                         {:bootstrap.servers (System/getenv "KAFKA_SERVERS")
                           :acks              "all"
                           :retries           1
                           :client.id         "test-producer"}
                          (serializers/long-serializer)
                          (doto
                            (KafkaAvroSerializer.)
-                           (.configure {"schema.registry.url" "http://schema-registry:8081"} false)))]
+                           (.configure {"schema.registry.url" (System/getenv "KAFKA_SCHEMA_REGISTRY")} false)))]
     (send-sync!
       producer
       {:topic (str "org.akvo.akvoflowsandbox.datapoint")
        :key   (get data-point "surveyId")
        :value (avro/->java DataPointSchema data-point)})))
 
-(defn tile-number [lat lon zoom]
-  (let [zoom-shifted (bit-shift-left 1 zoom)
-        lat-radians (Math/toRadians lat)
-        xtile (int (Math/floor (* (/ (+ 180 lon) 360) zoom-shifted)))
-        ytile (int (Math/floor (* (/ (- 1
-                                        (/
-                                          (Math/log (+ (Math/tan lat-radians)
-                                                       (/ 1 (Math/cos lat-radians))))
-                                          Math/PI))
-                                     2)
-                                  zoom-shifted)))]
-    (str zoom
-         "/"
-         (cond (< xtile 0) 0
-               (>= xtile zoom-shifted) (- zoom-shifted 1)
-               :else xtile)
-         "/"
-         (cond (< ytile 0) 0
-               (>= ytile zoom-shifted) (- zoom-shifted 1)
-               :else ytile))))
-
 (defonce http-client (http/create-client {:connection-timeout 10000
                                           :request-timeout    10000
                                           :max-connections    10}))
 
 (defn json-request [req]
-  (-> (http/proxy-request http-client
-                          (update req :headers merge {"content-type" "application/json"}))
-      (update :status :code)
-      (update :body (fn [body] (json/parse-string body true)))))
+  (info "req -> " req)
+  (let [res (-> (http/proxy-request http-client
+                                    (update req :headers merge {"content-type" "application/json"}))
+                (update :status :code)
+                (update :body (fn [body] (json/parse-string body true))))]
+    (info "resp -> " res)
+    res))
 
 (defn create-map [datapoint-id]
-  (json-request {:method :post
-                 :url    "http://flow-maps:3000/create-map"
-                 :body   (json/generate-string
-                           {:version "1.5.0",
-                            :layers  [{:type    "mapnik",
-                                       :options {:sql              (str "select * from datapoint where id='" datapoint-id "'"),
-                                                 :geom_column      "geom",
-                                                 :srid             4326,
-                                                 :cartocss         "#s { marker-width: 10; marker-fill: #e00050; }",
-                                                 :cartocss_version "2.0.0",
-                                                 :interactivity    "id"}}]})}))
+  (try-for 60
+           (json-request {:method :post
+                          :url    "http://flow-maps:3000/create-map"
+                          :body   (json/generate-string
+                                    {:version "1.5.0",
+                                     :layers  [{:type    "mapnik",
+                                                :options {:sql              (str "select * from datapoint where id='" datapoint-id "'"),
+                                                          :geom_column      "geom",
+                                                          :srid             4326,
+                                                          :cartocss         "#s { marker-width: 10; marker-fill: #e00050; }",
+                                                          :cartocss_version "2.0.0",
+                                                          :interactivity    "id"}}]})})))
 
 
 (deftest happy-path
          (let [datapoint-id (str (UUID/randomUUID))
-               _ (push-data-point {:identifier            datapoint-id
-                                   :survey-id             20
-                                   :latitude              (- (rand (- 160 0.00001)) 80)
-                                   :longitude             (- (rand (- 360 0.00001)) 180)
-                                   :created-date-time     (System/currentTimeMillis)
-                                   :last-update-date-time (System/currentTimeMillis)})
+               _ (info (push-data-point {:identifier            datapoint-id
+                                         :survey-id             20
+                                         :latitude              (- (rand (- 160 0.00001)) 80)
+                                         :longitude             (- (rand (- 360 0.00001)) 180)
+                                         :created-date-time     (System/currentTimeMillis)
+                                         :last-update-date-time (System/currentTimeMillis)}))
                response (create-map datapoint-id)
                layer-group (-> response :body :layergroupid)]
            (is (= 200 (:status response)))
