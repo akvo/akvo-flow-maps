501baf41b21570cccfe37d984aadf29dd6a9cc27
Dan Lebrero <dlebrero@gmail.com>
2017-11-02 00:22:06 +0100

    [#9] Running regular tests before building uberjar and integration tests after building docker images

--- ci/test/build.sh
+++ ci/test/build.sh
@@ -10,7 +10,7 @@ if [ -z "$TRAVIS_COMMIT" ]; then
 fi
 
 docker build --rm=false -t akvo-flow-dev:develop . -f Dockerfile-dev
-docker run -v $HOME/.m2:/root/.m2 -v `pwd`:/app akvo-flow-dev:develop lein uberjar
+docker run -v $HOME/.m2:/root/.m2 -v `pwd`:/app akvo-flow-dev:develop lein do test, uberjar
 
 ls -lrt target
 
@@ -21,7 +21,7 @@ docker build --rm=false -t eu.gcr.io/${PROJECT_NAME}/akvo-flow-maps-windshaft:$T
 docker tag eu.gcr.io/${PROJECT_NAME}/akvo-flow-maps-windshaft:$TRAVIS_COMMIT eu.gcr.io/${PROJECT_NAME}/akvo-flow-maps-windshaft:develop
 
 docker-compose -p akvo-flow-ci -f docker-compose-ci.yml up -d --build
-docker-compose -p akvo-flow-ci -f docker-compose-ci.yml run --no-deps tests lein test
+docker-compose -p akvo-flow-ci -f docker-compose-ci.yml run --no-deps tests lein test :integration
 
 rc=$?
 
