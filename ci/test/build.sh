d27610bfd22ad5da4d72a660bfc0cf2f80398d4b
Dan Lebrero <dlebrero@gmail.com>
2017-11-01 18:22:59 +0100

    [#9] Cannot get logs when build fails because later stages cannot resolve $IMAGE_TAG

--- ci/test/build.sh
+++ ci/test/build.sh
@@ -9,23 +9,19 @@ if [ -z "$TRAVIS_COMMIT" ]; then
     export TRAVIS_COMMIT=local
 fi
 
-IMAGE_TAG=$(echo -n "${TRAVIS_COMMIT}" | cut -c-8)
-IMAGE_NAME="akvo/akvo-maps:${IMAGE_TAG}"
-export IMAGE_TAG="${IMAGE_TAG}"
-
 docker build --rm=false -t akvo-flow-dev:develop . -f Dockerfile-dev
 docker run -v $HOME/.m2:/root/.m2 -v `pwd`:/app akvo-flow-dev:develop lein uberjar
 
 ls -lrt target
 
-docker build --rm=false -t eu.gcr.io/${PROJECT_NAME}/akvo-flow-maps:$IMAGE_TAG .
-docker tag eu.gcr.io/${PROJECT_NAME}/akvo-flow-maps:$IMAGE_TAG eu.gcr.io/${PROJECT_NAME}/akvo-flow-maps:develop
+docker build --rm=false -t eu.gcr.io/${PROJECT_NAME}/akvo-flow-maps:$TRAVIS_COMMIT .
+docker tag eu.gcr.io/${PROJECT_NAME}/akvo-flow-maps:$TRAVIS_COMMIT eu.gcr.io/${PROJECT_NAME}/akvo-flow-maps:develop
 
-docker build --rm=false -t eu.gcr.io/${PROJECT_NAME}/akvo-flow-maps-windshaft:$IMAGE_TAG ./windshaft
-docker tag eu.gcr.io/${PROJECT_NAME}/akvo-flow-maps-windshaft:$IMAGE_TAG eu.gcr.io/${PROJECT_NAME}/akvo-flow-maps-windshaft:develop
+docker build --rm=false -t eu.gcr.io/${PROJECT_NAME}/akvo-flow-maps-windshaft:$TRAVIS_COMMIT ./windshaft
+docker tag eu.gcr.io/${PROJECT_NAME}/akvo-flow-maps-windshaft:$TRAVIS_COMMIT eu.gcr.io/${PROJECT_NAME}/akvo-flow-maps-windshaft:develop
 
 docker-compose -p akvo-flow-ci -f docker-compose-ci.yml up -d --build
-docker-compose -p akvo-flow-ci -f docker-compose-ci.yml run tests lein test
+docker-compose -p akvo-flow-ci -f docker-compose-ci.yml run --no-deps tests lein test
 
 rc=$?
 
