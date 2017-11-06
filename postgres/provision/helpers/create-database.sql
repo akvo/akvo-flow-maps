c6247c5056aa7674099d5b310a78faf22b80c173
Dan Lebrero <dlebrero@gmail.com>
2017-11-06 18:44:24 +0100

    [#2] Not creating the tenant DB in the postgres setup script as it should be created automatically when consuming the first message

--- postgres/provision/helpers/create-database.sql
+++ postgres/provision/helpers/create-database.sql
@@ -4,10 +4,3 @@ WITH OWNER = dbuser
      ENCODING = 'UTF8'
      LC_COLLATE = 'en_US.UTF-8'
      LC_CTYPE = 'en_US.UTF-8';
-
-CREATE DATABASE a_tenant_db
-WITH OWNER = a_tenant_user
-     TEMPLATE = template0
-     ENCODING = 'UTF8'
-     LC_COLLATE = 'en_US.UTF-8'
-     LC_CTYPE = 'en_US.UTF-8';
