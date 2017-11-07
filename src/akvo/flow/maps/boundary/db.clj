e9c352e0f7abf58f52005a901cbb23521bb681c7
Dan Lebrero <dlebrero@gmail.com>
2017-11-07 17:17:11 +0100

    [#2] Hugs is x2 slower than plain jdbc. Not using it for batch inserts

--- src/akvo/flow/maps/boundary/db.clj
+++ src/akvo/flow/maps/boundary/db.clj
@@ -1,16 +1,12 @@
 (ns akvo.flow.maps.boundary.db
   (:require
     clojure.set
-    [integrant.core :as ig]
-    [clojure.java.jdbc :as jdbc]
-    ragtime.jdbc
-    [hugsql.core :as hugsql]
-    [clojure.string :as s]
-    [clojure.string :as string]
-    [hugsql.parameters :as parameters]))
+    [clojure.java.jdbc :as jdbc]))
 
-(hugsql/def-db-fns "akvo/flow/maps/boundary/datapoints.sql")
-(hugsql/def-sqlvec-fns "akvo/flow/maps/boundary/datapoints.sql")
+(comment
+  (jdbc/execute! (dev/db) ["DELETE from datapoint"])
+  (jdbc/query (dev/db) ["select count(*) from datapoint"])
+  )
 
 (defn ->db-timestamp [v]
   (when v
@@ -32,19 +28,15 @@
   (when-let [db-datapoints (->> datapoints
                                 (map ->db-value)
                                 (filter valid?)
+                                (map (juxt :id :survey-id :last-update-date-time :created-date-time :longitude :latitude
+                                           :survey-id :last-update-date-time :created-date-time :longitude :latitude :id))
                                 seq)]
-    (let [[x & xs] db-datapoints
-          [sql & first-row] (upsert-datapoint-sqlvec x)
-          other-rows (mapv (comp rest upsert-datapoint-sqlvec) xs)]
-      (jdbc/execute! db
-                     (into [sql first-row] other-rows)
-                     {:transaction? true
-                      :multi?       true}))))
-
-(comment
-  (time
-    (dotimes [_ 10]
-      (do
-        (insert-batch "jdbc:postgresql://postgres/avlkmasdlkvm1510061891540?user=dnxbtrsqco5sdnklrfkzbbvme4zyx9izqwjvs&password=pEdfR8_b4n2dDMAQl10O3hTDyBPhNk1d5vbFN"
-                      inserts)
-        :done))))
+    (jdbc/execute! db
+                   (into ["insert into datapoint(id, survey_id, created_date_time, last_update_date_time, geom)
+                                 values (?, ?, ?, ?, ST_SetSRID(ST_MakePoint(?, ?), 4326))
+                                 ON conflict(id)
+                         do update set (survey_id, created_date_time, last_update_date_time, geom) = (?, ?, ?, ST_SetSRID(ST_MakePoint(?, ?), 4326))
+                            where datapoint.id = ?"]
+                         db-datapoints)
+                   {:transaction? true
+                    :multi?       true})))
