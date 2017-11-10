e1250f7c23a97e73dbc952d685a028996890096d
Dan Lebrero <dlebrero@gmail.com>
2017-11-10 14:45:23 +0100

    [#5] Using sh instead of bash

--- maybe-import-and-java-jar.sh
+++ maybe-import-and-java-jar.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 if [ -f "/pg-certs/server.crt" ]; then
     keytool -import -trustcacerts -keystore /usr/lib/jvm/java-8-openjdk-amd64/jre/lib/security/cacerts -storepass changeit -noprompt -alias postgrescert -file /pg-certs/server.crt
