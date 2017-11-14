7c99c96b4fcb071eeff6f366de5f80c8db43725a
Dan Lebrero <dlebrero@gmail.com>
2017-11-14 15:27:34 +0100

    [#22] Wait until the old containers are not running

--- ci/test/wait-for-k8s-deployment-to-be-ready.sh
+++ ci/test/wait-for-k8s-deployment-to-be-ready.sh
@@ -9,14 +9,23 @@ while [ $(( $(date +%s) - 120 )) -lt ${starttime} ]; do
     consumer_status=`kubectl get pods --namespace flow-maps -l "flow-maps-version=$TRAVIS_COMMIT,run=flow-maps-consumer" -ao jsonpath='{range .items[*].status.containerStatuses[*]}{@.name}{" ready="}{@.ready}{"\n"}{end}'`
     windshaft_status=`kubectl get pods --namespace flow-maps -l "flow-maps-version=$TRAVIS_COMMIT,run=flow-maps-windshaft" -ao jsonpath='{range .items[*].status.containerStatuses[*]}{@.name}{" ready="}{@.ready}{"\n"}{end}'`
 
-    if [[ ${consumer_status} =~ "ready=true" ]] && [[ ${windshaft_status} =~ "ready=true" ]]; then
+# We want to make sure that when we hit the ingress from the integration test, we are hitting the new containers,
+# hence we wait until the old pods are gone.
+# Another possibility could be to check that the service is pointing just to the new containers.
+    old_consumer_status=`kubectl get pods --namespace flow-maps -l "flow-maps-version!=$TRAVIS_COMMIT,run=flow-maps-consumer" -ao jsonpath='{range .items[*].status.containerStatuses[*]}{@.name}{" ready="}{@.ready}{"\n"}{end}'`
+    old_windshaft_status=`kubectl get pods --namespace flow-maps -l "flow-maps-version!=$TRAVIS_COMMIT,run=flow-maps-windshaft" -ao jsonpath='{range .items[*].status.containerStatuses[*]}{@.name}{" ready="}{@.ready}{"\n"}{end}'`
+
+    if [[ ${consumer_status} =~ "ready=true" ]] && [[ ${windshaft_status} =~ "ready=true" ]] && ! [[ ${old_consumer_status} =~ "ready" ]] && ! [[ ${old_windshaft_status} =~ "ready" ]] ; then
         exit 0
     else
+        echo "Waiting for the containers to be ready"
         sleep 10
     fi
 done
 
-echo "Containers not ready after 2 minutes"
-echo $consumer_status
-echo $windshaft_status
+echo "Containers not ready after 2 minutes or old containers not stopped"
+
+kubectl get pods --namespace flow-maps -l "run=flow-maps-consumer" -ao jsonpath='{range .items[*].status.containerStatuses[*]}{@.name}{" ready="}{@.ready}{"\n"}{end}'
+kubectl get pods --namespace flow-maps -l "run=flow-maps-windshaft" -ao jsonpath='{range .items[*].status.containerStatuses[*]}{@.name}{" ready="}{@.ready}{"\n"}{end}'
+
 exit 1
