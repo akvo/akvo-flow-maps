a087b2c41617c13976cfb37a5b4bd90e57aad28b
Dan Lebrero <dlebrero@gmail.com>
2017-11-14 16:05:41 +0100

    [#22] Fixing path of the JVM CA store

--- maybe-import-and-java-jar.sh
+++ maybe-import-and-java-jar.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env sh
 
-echo "Debug CI"
-ls -lrt /pg-certs
 if [ -f "/pg-certs/server.crt" ]; then
-    keytool -import -trustcacerts -keystore /usr/lib/jvm/java-8-openjdk-amd64/jre/lib/security/cacerts -storepass changeit -noprompt -alias postgrescert -file /pg-certs/server.crt
+    keytool -import -trustcacerts -keystore /usr/lib/jvm/default-jvm/jre/lib/security/cacerts -storepass changeit -noprompt -alias postgrescert -file /pg-certs/server.crt
 fi
 
 java -jar /app/akvo-flow-maps.jar
