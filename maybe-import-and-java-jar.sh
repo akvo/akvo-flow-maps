4aa45709b2ef90b5da27cb8de6ee76b76017f803
Dan Lebrero <dlebrero@gmail.com>
2017-11-15 23:39:43 +0100

    [#24] SSL on CI also

--- maybe-import-and-java-jar.sh
+++ maybe-import-and-java-jar.sh
@@ -4,4 +4,11 @@ if [ -f "/pg-certs/server.crt" ]; then
     keytool -import -trustcacerts -keystore /usr/lib/jvm/default-jvm/jre/lib/security/cacerts -storepass changeit -noprompt -alias postgrescert -file /pg-certs/server.crt
 fi
 
-java -jar /app/akvo-flow-maps.jar
+JAVA_CERTS_OPTS=""
+
+if [ -f "/kafka-certs/client.p12" ]; then
+    keytool -importkeystore -srckeystore /kafka-certs/ca.truststore.jks -destkeystore /usr/lib/jvm/default-jvm/jre/lib/security/cacerts -srcstorepass qwerty -deststorepass changeit -srcalias caroot -destalias schemacert -noprompt
+    JAVA_CERTS_OPTS="-Djavax.net.ssl.keyStorePassword=asdfgh -Djavax.net.ssl.keyStore=/kafka-certs/client.p12 -Djavax.net.ssl.keyStoreType=pkcs12"
+fi
+
+java $JAVA_CERTS_OPTS -jar /app/akvo-flow-maps.jar
