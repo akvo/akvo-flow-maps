7cb89bb6f3c508796b12099985316b15a6dee7a7
Dan Lebrero <dlebrero@gmail.com>
2017-11-16 00:17:03 +0100

    [#24] Debugging CI

--- maybe-import-and-java-jar.sh
+++ maybe-import-and-java-jar.sh
@@ -7,11 +7,14 @@ fi
 JAVA_CERTS_OPTS=""
 
 echo "LSSSSsss"
-ls /kafka-certs
+ls -l /kafka-certs
 
 if [ -f "/kafka-certs/client.p12" ]; then
     keytool -importkeystore -srckeystore /kafka-certs/ca.truststore.jks -destkeystore /usr/lib/jvm/default-jvm/jre/lib/security/cacerts -srcstorepass qwerty -deststorepass changeit -srcalias caroot -destalias schemacert -noprompt
+    keytool -importkeystore -srckeystore /kafka-certs/ca.truststore.jks -destkeystore /usr/lib/jvm/default-jvm/jre/lib/security/cacerts -srcstorepass qwerty -deststorepass changeit -srcalias caroot -destalias schemacert -noprompt
     JAVA_CERTS_OPTS="-Djavax.net.ssl.keyStorePassword=asdfgh -Djavax.net.ssl.keyStore=/kafka-certs/client.p12 -Djavax.net.ssl.keyStoreType=pkcs12"
+else
+    echo "cert file not found"
 fi
 
 java $JAVA_CERTS_OPTS -jar /app/akvo-flow-maps.jar
