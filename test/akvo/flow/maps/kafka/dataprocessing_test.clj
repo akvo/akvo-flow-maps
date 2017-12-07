306efb33024bccc8b6d3ba6adab0676bb5c0484a
Dan Lebrero <dlebrero@gmail.com>
2017-12-07 15:56:26 +0100

    [#21] clean up tests [skip ci]

--- test/akvo/flow/maps/kafka/dataprocessing_test.clj
+++ test/akvo/flow/maps/kafka/dataprocessing_test.clj
@@ -3,98 +3,58 @@
     [akvo.flow.maps.kafka.datapoint-processing :as dp]
     [clojure.test :refer :all]))
 
+(defn kafka-message [topic value]
+  {:topic     topic,
+   :partition 0,
+   :offset    24,
+   :key       nil,
+   :value     value})
+
+(defn datapoint [id]
+  {:identifier            id,
+   :survey-id             20,
+   :longitude             50.82581103065513,
+   :latitude              -18.952741795895086,
+   :created-date-time     1509724347835,
+   :last-update-date-time 1509724347835})
+
+(defn row [id]
+  {:identifier            id,
+   :survey-id             20,
+   :longitude             50.82581103065513,
+   :latitude              -18.952741795895086,
+   :created-date-time     #inst "2017-11-03T15:52:27.835-00:00",
+   :last-update-date-time #inst "2017-11-03T15:52:27.835-00:00"})
+
 (deftest happy-path
   (let [actions (dp/actions {"topic-a.datapoint" {:db-creation-state "done"}}
-                            [{:topic     "topic-a.datapoint",
-                              :partition 0,
-                              :offset    24,
-                              :key       nil,
-                              :value     {:identifier            "id0",
-                                          :survey-id             20,
-                                          :longitude             50.82581103065513,
-                                          :latitude              -18.952741795895086,
-                                          :created-date-time     1509724347835,
-                                          :last-update-date-time 1509724347835}}
-                             {:topic     "topic-b.datapoint",
-                              :partition 0,
-                              :offset    25,
-                              :key       nil,
-                              :value     {:identifier            "id1",
-                                          :survey-id             20,
-                                          :longitude             50.82581103065513,
-                                          :latitude              -18.952741795895086,
-                                          :created-date-time     1509724347835,
-                                          :last-update-date-time 1509724347835}}
-                             {:topic     "topic-a.datapoint",
-                              :partition 0,
-                              :offset    26,
-                              :key       nil,
-                              :value     {:identifier            "id2",
-                                          :survey-id             20,
-                                          :longitude             50.82581103065513,
-                                          :latitude              -18.952741795895086,
-                                          :created-date-time     1509724347835,
-                                          :last-update-date-time 1509724347835}}])]
+                            [(kafka-message "topic-a.datapoint" (datapoint "id0"))
+                             (kafka-message "topic-b.datapoint" (datapoint "id1"))
+                             (kafka-message "topic-a.datapoint" (datapoint "id2"))])]
+
     (is (= [[:upsert {:tenant "topic-a.datapoint"
-                      :rows   [{:identifier            "id0",
-                                :survey-id             20,
-                                :longitude             50.82581103065513,
-                                :latitude              -18.952741795895086,
-                                :created-date-time     #inst "2017-11-03T15:52:27.835-00:00",
-                                :last-update-date-time #inst "2017-11-03T15:52:27.835-00:00"}
-                               {:identifier            "id2",
-                                :survey-id             20,
-                                :longitude             50.82581103065513,
-                                :latitude              -18.952741795895086,
-                                :created-date-time     #inst "2017-11-03T15:52:27.835-00:00",
-                                :last-update-date-time #inst "2017-11-03T15:52:27.835-00:00"}]}]
+                      :rows   [(row "id0")
+                               (row "id2")]}]
             [:stats {:topic "topic-a.datapoint" :discarded 0 :upsert 2}]
+
             [:create-db {:tenant "topic-b.datapoint"}]
             [:upsert {:tenant "topic-b.datapoint"
-                      :rows   [{:identifier            "id1",
-                                :survey-id             20,
-                                :longitude             50.82581103065513,
-                                :latitude              -18.952741795895086,
-                                :created-date-time     #inst "2017-11-03T15:52:27.835-00:00",
-                                :last-update-date-time #inst "2017-11-03T15:52:27.835-00:00"}]}]
+                      :rows   [(row "id1")]}]
             [:stats {:topic "topic-b.datapoint" :discarded 0 :upsert 1}]]
            actions))))
 
 
 
 (deftest do-nothing-if-all-rows-are-invalid
-  (let [actions (dp/actions {} [{:topic     "topic-a.datapoint",
-                                 :partition 0,
-                                 :offset    24,
-                                 :key       nil,
-                                 :value     {:identifier            "id0",
-                                             :survey-id             20,
-                                             :longitude             nil,
-                                             :latitude              -18.952741795895086,
-                                             :created-date-time     1509724347835,
-                                             :last-update-date-time 1509724347835}}])]
+  (let [actions (dp/actions {} [(kafka-message "topic-a.datapoint" (assoc (datapoint "any id") :longitude nil))])]
     (is (= [[:stats {:topic "topic-a.datapoint" :discarded 1 :upsert 0}]]
            actions))))
 
 (deftest create-db-if-it-is-not-created-yet
   (let [actions (dp/actions {"topic-a.datapoint" {:db-creation-state "creating"}}
-                            [{:topic     "topic-a.datapoint",
-                              :partition 0,
-                              :offset    24,
-                              :key       nil,
-                              :value     {:identifier            "id0",
-                                          :survey-id             20,
-                                          :longitude             130,
-                                          :latitude              -18.952741795895086,
-                                          :created-date-time     1509724347835,
-                                          :last-update-date-time 1509724347835}}])]
+                            [(kafka-message "topic-a.datapoint" (datapoint "id0"))])]
     (is (= [[:create-db {:tenant "topic-a.datapoint"}]
             [:upsert {:tenant "topic-a.datapoint"
-                      :rows   [{:identifier            "id0",
-                                :survey-id             20,
-                                :longitude             130,
-                                :latitude              -18.952741795895086,
-                                :created-date-time     #inst "2017-11-03T15:52:27.835-00:00",
-                                :last-update-date-time #inst "2017-11-03T15:52:27.835-00:00"}]}]
+                      :rows   [(row "id0")]}]
             [:stats {:topic "topic-a.datapoint" :discarded 0 :upsert 1}]]
            actions))))
