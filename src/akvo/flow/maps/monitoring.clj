b76f6d45ccc9e000351464ae495c4eb505d889e0
Dan Lebrero <dlebrero@gmail.com>
2017-12-12 16:17:03 +0100

    [#13] Adding GC stats
    The default GC stats uses a Summary type, which is not supported by prometheus-to-sd

--- src/akvo/flow/maps/monitoring.clj
+++ src/akvo/flow/maps/monitoring.clj
@@ -2,19 +2,37 @@
   (:require [iapetos.core :as prometheus]
             [iapetos.collector.jvm :as jvm]
             [iapetos.collector.ring :as ring]
-            [integrant.core :as ig])
-  (:import (io.prometheus.client.hotspot DefaultExports)))
+            [integrant.core :as ig]
+            [iapetos.collector :as collector])
+  (:import (java.lang.management ManagementFactory GarbageCollectorMXBean)
+           (io.prometheus.client Collector CounterMetricFamily)))
+
+(defn gc-stats-collector []
+  (collector/named
+    {:namespace "iapetos_internal"
+     :name      "jvm_standard"}
+    (let [garbage-collectors (ManagementFactory/getGarbageCollectorMXBeans)]
+      (proxy [Collector]
+             []
+        (collect []
+          (let [gc-metrics-sum (CounterMetricFamily. "jvm_gc_collection_seconds_sum" "Time spent in a given JVM garbage collector in seconds." ["gc"])
+                gc-metrics-count (CounterMetricFamily. "jvm_gc_collection_seconds_count" "Amount of JVM garbage collector run" ["gc"])]
+            (doseq [^GarbageCollectorMXBean gc garbage-collectors]
+              (.addMetric gc-metrics-sum [(str (.getName gc) "_sum")] (double (.getCollectionTime gc)))
+              (.addMetric gc-metrics-count [(str (.getName gc) "_count")] (double (.getCollectionCount gc))))
+            [gc-metrics-sum gc-metrics-count]))))))
 
 (defmethod ig/init-key ::middleware [_ config]
   (let [registry (-> (prometheus/collector-registry)
                      (prometheus/register
                        (jvm/standard)
                        (jvm/memory-pools)
-                       (jvm/threads))
+                       (jvm/threads)
+                       (gc-stats-collector))
                      #_(prometheus/register
-                       (prometheus/histogram :app/duration-seconds)
-                       (prometheus/gauge :app/active-users-total)
-                       (prometheus/counter :app/runs-total))
+                         (prometheus/histogram :app/duration-seconds)
+                         (prometheus/gauge :app/active-users-total)
+                         (prometheus/counter :app/runs-total))
                      (ring/initialize))]
     #(-> %
          (ring/wrap-metrics registry))))
