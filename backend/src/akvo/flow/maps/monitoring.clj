7c26b3fb2ad5016bff177308fa4dbd793dab4824
Dan Lebrero <dlebrero@gmail.com>
2017-12-27 11:41:51 +0100

    [#13] With Prometheus we do not need to circumvent StackDriver lack of support for "summary" types

--- backend/src/akvo/flow/maps/monitoring.clj
+++ backend/src/akvo/flow/maps/monitoring.clj
@@ -3,26 +3,7 @@
             [iapetos.collector.jvm :as jvm]
             [iapetos.collector.ring :as ring]
             [integrant.core :as ig]
-            [iapetos.collector :as collector]
-            [iapetos.registry :as registry]
-            [iapetos.collector.exceptions :as ex])
-  (:import (java.lang.management ManagementFactory GarbageCollectorMXBean)
-           (io.prometheus.client Collector CounterMetricFamily)))
-
-(defn gc-stats-collector []
-  (collector/named
-    {:namespace "iapetos_internal"
-     :name      "jvm_standard"}
-    (let [garbage-collectors (ManagementFactory/getGarbageCollectorMXBeans)]
-      (proxy [Collector]
-             []
-        (collect []
-          (let [gc-metrics-sum (CounterMetricFamily. "jvm_gc_collection_seconds_sum" "Time spent in a given JVM garbage collector in seconds." ["gc"])
-                gc-metrics-count (CounterMetricFamily. "jvm_gc_collection_seconds_count" "Amount of JVM garbage collector run" ["gc"])]
-            (doseq [^GarbageCollectorMXBean gc garbage-collectors]
-              (.addMetric gc-metrics-sum [(.getName gc)] (double (.getCollectionTime gc)))
-              (.addMetric gc-metrics-count [(.getName gc)] (double (.getCollectionCount gc))))
-            [gc-metrics-sum gc-metrics-count]))))))
+            [iapetos.collector.exceptions :as ex]))
 
 (defn wrap-health-check
   [handler]
@@ -33,11 +14,8 @@
 
 (defmethod ig/init-key ::collector [_ config]
   (-> (prometheus/collector-registry)
+      (jvm/initialize)
       (prometheus/register
-        (jvm/standard)
-        (jvm/memory-pools)
-        (jvm/threads)
-        (gc-stats-collector)
         (prometheus/counter :datapoint/process {:labels [:topic :name]})
         (prometheus/histogram
           :fn/duration-seconds
